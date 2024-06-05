41c12a2b48e5fdf6cda20e2bfa36d9d3d784f489
Yuri-Campos <ygsoares20@gmail.com>
2024-06-05 00:29:00 -0300

    update dataset loading + game sidebar

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import os
-
+from helpers.datasets import json_to_list
 
 st.set_page_config(
     layout='wide',
@@ -9,10 +9,25 @@ st.set_page_config(
 )
 
 
+
 @st.cache_data
 def load_data(dataset):
-    df = pd.read_csv(f'data/{dataset}.csv')
+    df = pd.read_csv(f'data/datasets/{dataset}.csv')
     return df
 
 
-st.selectbox('Game', )
+games = json_to_list()
+
+fifa = st.sidebar.selectbox('Game', games)
+
+df = load_data(fifa)
+df.set_index('Name', inplace = True)
+
+clubs = df['Club'].value_counts().index
+club = st.selectbox('Clube', clubs)
+df_filtered = df[df['Club'] == club]
+
+show_bar = st.checkbox('Show')
+if show_bar:
+    st.bar_chart(df_filtered['Overall'])
+    st.write(fifa)
