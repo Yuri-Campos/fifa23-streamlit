4c85be7f1b5142eee9e5643a1dd8b15a91647137
Yuri-Campos <ygsoares20@gmail.com>
2024-06-05 00:28:35 -0300

    update dataset name extraction method

--- helpers/datasets.py
+++ helpers/datasets.py
@@ -1,12 +1,23 @@
 import os
 import json
 
-folder_path = 'data/'
+folder_path = 'data/datasets/'
 
 def dataset_names():
+    files = []
     file_list = os.listdir(folder_path)
-    print(file_list)
-    with open('data/datasets', 'w') as json_file:
-        json.dump(file_list, json_file, indent=4)
+    for file in file_list:
+        name, ext = os.path.splitext(file)
+        files.append(name)
+    print(files)
+    with open('data/json/datasets.json', 'w') as json_file:
+        json.dump(files, json_file, indent=4)
+
+
+def json_to_list():
+    with open('data/json/datasets.json', 'r') as json_file:
+        data = json.load(json_file)
+    return list(data)
+
 
 dataset_names()
