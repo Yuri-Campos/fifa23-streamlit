4de8f4a43f685e901c0c5349bc3532229e29fc9d
Yuri-Campos <ygsoares20@gmail.com>
2024-06-06 01:58:38 -0300

    add dataframe column config

--- pages/3_teams.py
+++ pages/3_teams.py
@@ -11,4 +11,24 @@ if 'data' not in st.session_state:
     st.write('No dataset found: Please, go to settings and import a dataset')
 else:
     df = st.session_state['data']
-    st.write(df)
+    clubs = df['Club'].value_counts().index
+    club = st.sidebar.selectbox('Clubs', clubs)
+    df_filtered = df[(df['Club'] == club)].set_index('Name')
+
+    st.image(df_filtered.iloc[0]['Club Logo'])
+    st.markdown(f"## {club}")
+
+    columns = ['Age', 'Photo', 'Flag', 'Overall', 'Value(£)','Wage(£)', 'Joined', 'Height(cm.)', 'Weight(lbs.)', 'Contract Valid Until', 'Release Clause(£)']
+
+
+    st.dataframe(df_filtered[columns], 
+                 column_config={
+                     'Overall': st.column_config.ProgressColumn(
+                         'Overall', min_value=0, max_value=100, format='%d'
+                     ),
+                     'Wage(£)': st.column_config.ProgressColumn(
+                         'Weekly Age', min_value=0, max_value=df_filtered['Wage(£)'].max(), format='£%f'
+                     ),
+                     'Photo': st.column_config.ImageColumn(),
+                     'Flag': st.column_config.ImageColumn('Country Flag')
+                 })
